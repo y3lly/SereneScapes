96ab8802eeb0749c11f81700e759608cb85f91a6
Mariel Jenine <155594590+y3lly@users.noreply.github.com>
2024-05-31 10:55:12 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var loginForm = document.getElementById('loginForm');
+    document.getElementById('loginButton').addEventListener('click', function(event) {
+        event.preventDefault();
+        var username = document.getElementById('loginUsername').value;
+        var password = document.getElementById('loginPassword').value;
+        if (username === '') {
+            // If username is empty
+            alert('Please enter username');
+        } else if (password === '') {
+            // If password is empty
+            alert('Please enter password');
+        } else if (username !== 'mariel') {
+            // If username is incorrect
+            alert('Incorrect username');
+        } else if (password !== 'mjma') {
+            // If password is incorrect
+            alert('Incorrect password');
+        } else {
+            // If both username and password are correct
+            window.location.href = "index.html";
+        }
+    });
+    
+});
